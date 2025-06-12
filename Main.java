80ca0498d1ad6c3e1666471fef4cb6b5ad817895
Ignacio David Menares Perez <ignacio.menares@mail.udp.cl>
2025-06-11 21:24:44 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
-
 import java.util.*;
-
+import java.text.SimpleDateFormat;
 public class Main {
     public static void main(String[] args) throws Exception {
         long inicio = System.currentTimeMillis() / 1000;
         List<Paciente> pacientes = GeneradorPacientes.generar(144, inicio);
         GeneradorPacientes.guardarArchivo(pacientes, "Pacientes_24h.txt");
-
         SimuladorUrgencia simulador = new SimuladorUrgencia(pacientes);
         simulador.simular(144);
+        SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm");
+        for (Paciente p : pacientes) {
+            Date horaLlegada = new Date(p.getTiempoLlegada() * 1000);
+            String horaStr = formatoHora.format(horaLlegada);
+            System.out.printf("%s\t%s\t\tC%d\t\t%s\t\t%s\n",
+                    p.getNombre(),
+                    p.getApellido(),
+                    p.getCategoria(),
+                    horaStr,
+                    "-");
+        }
     }
 }
